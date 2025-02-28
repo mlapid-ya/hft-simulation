8ecef12afe12c0255e1ab81713ccf46ceff6249e
mlapid-ya <m.lapid@yandex.ru>
2025-02-28 14:34:55 +0100

    Updated order book data class

--- src/hft_simulation/shared_utils/order_book.py
+++ src/hft_simulation/shared_utils/order_book.py
@@ -1,4 +1,5 @@
 from typing import Annotated
+from datetime import datetime
 from collections import namedtuple
 
 from loguru import logger
@@ -14,15 +15,25 @@ class OrderBook(BaseModel):
         arbitrary_types_allowed=True
     )
 
-    timestamp:       Annotated[float, Field(alias='timestamp')]
-    change_id:       Annotated[int, Field(alias='change_id')]
+    timestamp:       Annotated[datetime, Field(alias='timestamp')]
     instrument_name: Annotated[str, Field(alias='instrument_name')]
     bids:            Annotated[list[Level[float, float]], Field(alias='bids')]
     asks:            Annotated[list[Level[float, float]], Field(alias='asks')]
 
     def model_post_init(self, __context):
         if len(self.bids) != len(self.asks):
-           logger.error(f"Number of bids and asks must be equal, got {len(self.bids)} != {len(self.asks)}")
+           logger.error(f'Number of bids and asks must be equal, got {len(self.bids)} != {len(self.asks)}')
+
+    @field_validator('timestamp', mode='before')
+    def convert_timestamp_to_datetime(cls, timestamp: float | datetime | str) -> datetime:
+        if isinstance(timestamp, float):
+            return datetime.fromtimestamp(timestamp, tz=None)
+        elif isinstance(timestamp, datetime):
+            return timestamp
+        elif isinstance(timestamp, str):
+            return datetime.fromisoformat(timestamp)
+        else:
+            raise ValueError(f'Invalid timestamp: {timestamp}')
 
     @field_validator('bids', mode='before')
     def convert_bids_to_levels(cls, v: list[list[float, float]]) -> list[Level[float, float]]:
@@ -37,14 +48,14 @@ class OrderBook(BaseModel):
     def validate_bids(cls, v: list[Level[float, float]]) -> list[Level[float, float]]:
         for i in range(len(v) - 1):
             if v[i].price < v[i + 1].price:
-                logger.error(f"Bids are not sorted: {v[i].price} < {v[i + 1].price}")
+                logger.error(f'Bids are not sorted: {v[i].price} < {v[i + 1].price}')
         return v
 
     @field_validator('asks', mode='after')
     def validate_asks(cls, v: list[Level[float, float]]) -> list[Level[float, float]]:
         for i in range(len(v) - 1):
             if v[i].price > v[i + 1].price:
-                logger.error(f"Asks are not sorted: {v[i].price} > {v[i + 1].price}")
+                logger.error(f'Asks are not sorted: {v[i].price} > {v[i + 1].price}')
         return v
     
     @property
@@ -73,7 +84,7 @@ class OrderBook(BaseModel):
     
     @property
     def spread(self) -> float:
-        return self.ask_price - self.bid_price
+        return round(self.ask_price - self.bid_price, 4)
     
     @property
     def bid_volume_total(self) -> float:
@@ -84,8 +95,26 @@ class OrderBook(BaseModel):
         return sum(level.volume for level in self.asks)
     
     @property
+    def volume_imbalance(self) -> float:
+        return (self.bid_volume - self.ask_volume) / (self.bid_volume + self.ask_volume)
+    
+    @property
     def volume_imbalance_total(self) -> float:
         return (self.bid_volume_total - self.ask_volume_total) / (self.bid_volume_total + self.ask_volume_total)
+    
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__}(' \
+            f'timestamp={self.timestamp}, ' \
+            f'instrument_name={self.instrument_name}, ' \
+            f'bid_price={self.bid_price}, ' \
+            f'ask_price={self.ask_price}, ' \
+            f'mid_price={self.mid_price}, ' \
+            f'spread={self.spread}, ' \
+            f'volume_imbalance={self.volume_imbalance} , ' \
+            f'volume_imbalance_total={self.volume_imbalance_total})'
+
+    def __repr__(self) -> str:
+        return self.__str__()
 
     def get_volume_imbalance(self, lvl: int) -> float:
         if not (1 <= lvl <= self.depth):
